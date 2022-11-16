47dc5d0f8b89e1e5e425744f29c6a71372787f99
juan Manuel <juan.seoane@adgs.com>
2022-11-16 12:30:23 -0300

    second Test
    .

--- IntegrationTest/Tests/UnitTest1.cs
+++ IntegrationTest/Tests/UnitTest1.cs
@@ -23,11 +23,8 @@ namespace Tests
         [Fact]
         public  void  ItExists()
         {
-            var mockBDG = new Mock<IBDG>();
-            mockBDG.Setup(M => M.GetList()).Returns(new List<string> { "hola", "mundo", "goat" });
 
-            DemoController dmc = new DemoController(mockBDG.Object);
-
+            DemoController dmc = GetStdController();
             var result = dmc.get("hola");
 
             var isok = result as ContentResult;
@@ -36,6 +33,34 @@ namespace Tests
             var retval = isok.Content as string;
 
             Assert.Equal("Exists", retval);
+        }
+        [Fact]
+        public void NotExsts()
+        {
+            DemoController dmc = GetStdController();
+            var result = dmc.get("Perón");
+            var cnt = result as ContentResult;
+            var nfr = result as NotFoundResult;
+
+            Assert.Null(cnt);
+            Assert.NotNull(nfr);
+
+            var stdcode = nfr.StatusCode;
+
+            Assert.Equal(404, stdcode);
+
+          
+
+        }
+
+        private DemoController GetStdController()
+        {
+            var mockBDG = new Mock<IBDG>();
+            mockBDG.Setup(M => M.GetList()).Returns(new List<string> { "hola", "mundo", "goat" });
+
+            DemoController dmc = new DemoController(mockBDG.Object);
+
+            return dmc;
         }
     }
 }
